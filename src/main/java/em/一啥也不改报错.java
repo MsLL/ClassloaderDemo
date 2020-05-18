16a32caa9107a09112126c5ef4af1d8c32a09308
upup-ubuntu <2213311288@qq.com>
2020-05-18 14:15:08 +0800

    把.iml文件也加进来了，可以在里面直接调整orderEntry的顺序

--- src/main/java/em/一啥也不改报错.java
+++ src/main/java/em/一啥也不改报错.java
@@ -6,6 +6,7 @@ import com.company.*;
  * Created by Frank on 2019/9/22.
  */
 //NOTE-UPUP 2020/5/17 下午11:08 : 类名真用真用中文名--有时候也可以以，
+//NOTE-UPUP 2020/5/18 上午5:49 : 可以通过调整classpath中A.jar和B.jar的顺序来确定用哪个。eclipse中可以，idea自然也可以。或者直接在.iml文件中调orderEntry顺序---666
 public class 一啥也不改报错 {
     public static void main(String[] args) {
         new A().run();
